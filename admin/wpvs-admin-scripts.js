23bae4102a6fdcc422182ec49943f1f16f1a2e09
Martin Stewart <corgdesign@gmail.com>
2017-05-27 07:14:51 +0100

    Change file structure. Icon updating.

--- admin/wpvs-admin-scripts.js
+++ admin/wpvs-admin-scripts.js
@@ -2,11 +2,13 @@ jQuery(document).ready(function($){
     
 
 	// Update icon
-	$( '#wpvs_fa_icon' ).change(function(){
-		var chosen_icon = 'fa ' + $( this ).val();
+	$( '.iconpicker-item' ).on('click', function(){
+		
+		var chosen_icon =  $( this ).children( 'i' ).attr( 'class' );
 
 		$( '#wpvs_fa_chosen_icon' ).attr( 'class', '');
 		$( '#wpvs_fa_chosen_icon' ).attr( 'class', chosen_icon );
+		$( '#wpvs_fa_chosen_icon' ).hide().fadeIn();
 
 		console.log(chosen_icon);
 
--- wpvs.php
+++ wpvs.php
@@ -9,8 +9,6 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
 defined( 'ABSPATH' );
-include 'options.php';
-
 
 #************************************
 #		CSS and javascript
@@ -39,9 +37,70 @@ add_action( 'admin_enqueue_scripts', 'wpvs_enqueue_admin_scripts' );
 #			Admin menu
 #************************************
 
-# Add an admin menu, function wpvs_plugin_options() in options.php
+# Add an admin menu
 function wpvs_admin_menu() {
 	add_options_page( 'WP Visual Sitemap Settings', 'WP Visual Sitemap', 'manage_options', 'wpvs_settings', 'wpvs_plugin_options' );
+}
+
+# Output the options
+function wpvs_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+
+	?>
+	<div class="wrap">
+		<h1>WP Visual Sitemap Settings</h1>
+		<form method="post" action="options.php">
+			<?php
+
+				settings_fields( 'wpvs_option_group' );
+				do_settings_sections( 'wpvs_option_group' ); ?>
+
+				<table class="form-table">
+			        <tr valign="top">
+			        	<th scope="row">Background colour</th>
+			        	<td>
+			        		<input type="text" name="icon_background_colour" class="my-color-field" data-default-color="#009691" value="<?php echo esc_attr( get_option('icon_background_colour') ); ?>" />
+			        	</td>
+			        </tr>
+			         
+			        <tr valign="top">
+			        	<th scope="row">Use icons?</th>
+			        	<td>
+			        		<input type="checkbox" name="use_icons" value="yes" <?php checked( 'yes', get_option( 'use_icons' ) ); ?> />
+			        	</td>
+			        </tr>
+
+			        <tr valign="top">
+			        	<th scope="row">Number of columns</th>
+			        	<td>
+			        		
+			        		<label for="column_1">1</label>
+			        		<input type="radio" name="number_of_columns" value="1" id="column_1" <?php checked( '1', get_option( 'number_of_columns' ) ); ?> >
+
+			        		<label for="column_2">2</label>
+			        		<input type="radio" name="number_of_columns" value="2" id="column_2" <?php checked( '2', get_option( 'number_of_columns' ) ); ?> >
+
+			        		<label for="column_3">3</label>
+			        		<input type="radio" name="number_of_columns" value="3" id="column_3" <?php checked( '3', get_option( 'number_of_columns' ) ); ?> >
+
+			        		<label for="column_4">4</label>
+			        		<input type="radio" name="number_of_columns" value="4" id="column_4" <?php checked( '4', get_option( 'number_of_columns' ) ); ?> >
+
+			        	</td>
+			        </tr>
+			        
+			    </table>		    
+
+				<?php
+
+				submit_button();
+
+			?>
+		</form>
+	</div>
+<?php
 }
 
 # Register options
@@ -108,9 +167,10 @@ function wpvs_page_settings($post) {
 	</p>
 
 	<?php 
-	if ( isset ( $wpvs_stored_meta['wpvs_fa_icon'] ) ) {
-		echo '<i id="wpvs_fa_chosen_icon" class="fa ' . $wpvs_stored_meta['wpvs_fa_icon'][0] . '"></i>';
-	} ?>
+	
+	echo isset ( $wpvs_stored_meta['wpvs_fa_icon'] ) ? '<i id="wpvs_fa_chosen_icon" class="fa ' . $wpvs_stored_meta['wpvs_fa_icon'][0] . '"></i>' : '<i id="wpvs_fa_chosen_icon"></i>';
+
+	?>
     <input type="text" name="wpvs_fa_icon" id="wpvs_fa_icon" data-input-search="true" placeholder="Search for icon..." value="<?php if ( isset ( $wpvs_stored_meta['wpvs_fa_icon'] ) ) echo $wpvs_stored_meta['wpvs_fa_icon'][0]; ?>">
 
     <script type="text/javascript">
