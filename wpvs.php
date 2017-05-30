c735d9ff9dafa89116bf3e9a0cb48cf2dee44732
Martin Stewart <corgdesign@gmail.com>
2017-05-30 12:58:02 +0100

    Updating plugin

--- wpvs.php
+++ wpvs.php
@@ -8,7 +8,9 @@ License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 #************************************
 #		CSS and javascript
